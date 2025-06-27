5c03435c5e05b28c6653820339c4823979e6fa70
Christian Klein <Sector7@gmx.de>
2025-06-27 17:30:34 +0200

    added local mirror & added local pacman cache to speed up builds

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -17,5 +17,14 @@ docker build -f Dockerfile.makepkg -t arch-makepkg .
 docker run --rm \
   -v "$build_dir":/home/builder/pkgbuild \
   -v "$HOME/.makepkg.conf":/home/builder/.makepkg.conf:ro \
+  -v /var/cache/pacman/pkg:/var/cache/pacman/pkg:ro \
+  -e MIRROR_URL="$MIRROR_URL" \
   arch-makepkg \
-  bash -c "cd /home/builder/pkgbuild && makepkg -fs --noconfirm && rm -f src/*.zip"
+  bash -c ' \
+    if [[ -n "$MIRROR_URL" ]]; then \
+      echo "==> Using custom mirror: $MIRROR_URL"; \
+      echo "Server = $MIRROR_URL" | sudo tee /etc/pacman.d/mirrorlist; \
+    else \
+      echo "==> Using default Arch mirrorlist"; \
+    fi && \
+    cd /home/builder/pkgbuild && makepkg -fs --noconfirm'
